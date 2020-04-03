001102df8a8348149e35c46017bd5394459a586e
Josh Worden <solocommand@gmail.com>
2020-04-03 13:48:27 -0500

    Add primary section context map

--- sites/forconstructionpros.com/config/navigation.js
+++ sites/forconstructionpros.com/config/navigation.js
@@ -1,6 +1,103 @@
 module.exports = {
   primary: {
+    contextual: true,
     items: [
+      {
+        href: '/equipment',
+        label: 'Equipment',
+        children: [
+          {
+            href: '/equipment/attachments',
+            label: 'Attachments',
+          },
+          {
+            href: '/equipment/compaction',
+            label: 'Compaction',
+          },
+          {
+            href: '/equipment/earthmoving',
+            label: 'Earthmoving',
+          },
+          {
+            href: '/equipment/fleet-maintenance',
+            label: 'Fleet Maintenance',
+          },
+          {
+            href: '/equipment/snow-removal-equipment',
+            label: 'Snow Removal',
+          },
+        ],
+      },
+      {
+        href: '/rental',
+        label: 'Rental',
+        children: [
+          {
+            href: '/rental/lifting-equipment',
+            label: 'Lifting Equipment',
+          },
+        ],
+      },
+      {
+        href: '/business',
+        label: 'Business',
+        children: [
+          {
+            href: '/business/business-services',
+            label: 'Business Services',
+          },
+        ],
+      },
+      {
+        href: '/concrete',
+        label: 'Concrete',
+        children: [
+          {
+            href: '/concrete/decorative',
+            label: 'Decorative',
+          },
+        ],
+      },
+      {
+        href: '/asphalt',
+        label: 'Asphalt',
+        children: [
+          {
+            href: '/asphalt/additives',
+            label: 'Additives',
+          },
+        ],
+      },
+      {
+        href: '/pavement-maintenance',
+        label: 'Pavement Maintenance',
+        children: [
+          {
+            href: '/pavement-maintenance/marking-striping',
+            label: 'Marking & Striping',
+          },
+        ],
+      },
+      {
+        href: '/profit-matters',
+        label: 'Profit Matters',
+        children: [
+        ],
+      },
+      {
+        href: '/equipment-management',
+        label: 'Equipment Management',
+        children: [
+          {
+            href: '/equipment-management/fleet-news',
+            label: 'Fleet News',
+          },
+          {
+            href: '/equipment-management/management-standards',
+            label: 'Management Standards',
+          },
+        ],
+      },
     ],
   },
   secondary: {
@@ -45,8 +142,8 @@ module.exports = {
       label: 'Topics',
       items: [
         { href: '/equipment', label: 'Equipment' },
-        { href: '/trucks', label: 'Trucks' },
         { href: '/rental', label: 'Rental' },
+        { href: '/business', label: 'Business' },
         { href: '/concrete', label: 'Concrete' },
         { href: '/asphalt', label: 'Asphalt' },
         { href: '/pavement-maintenance', label: 'Pavement Maintenance' },
@@ -61,7 +158,6 @@ module.exports = {
         { href: '/asphalt-industry-directory', label: 'Asphalt Industry Directory' },
         { href: '/economics', label: 'Economics' },
         { href: '/construction-technology', label: 'Technology' },
-        { href: '/business', label: 'Business' },
         { href: '/specguide', label: 'Spec Guides' },
         { href: '/video', label: 'Video Network' },
         { href: '/magazine', label: 'Magazine' },
