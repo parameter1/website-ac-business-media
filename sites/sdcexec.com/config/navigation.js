9989e71187fd0a9aeed8861056359e76104b4687
Josh Worden <josh@limit0.io>
2020-06-03 13:20:29 -0500

    Replace Learning Center with Magazine

--- sites/sdcexec.com/config/navigation.js
+++ sites/sdcexec.com/config/navigation.js
@@ -14,13 +14,13 @@ module.exports = {
   },
   secondary: {
     items: [
+      { href: '/magazine', label: 'Magazine' },
       { href: '/awards', label: 'Awards' },
       { href: '/events', label: 'Events' },
       { href: '/blogs', label: 'Expert Columnists' },
       { href: 'http://www.supplychainnetworkmediakit.com/', label: 'Advertise', target: '_blank' },
       { href: '/contact-us', label: 'Contact Us' },
       { href: '/webinars', label: 'Webinars' },
-      { href: 'http://www.supplychainlearningcenter.com/', label: 'Learning Center', target: '_blank' },
       { href: 'https://sdcexec.tradepub.com/?pt=dir&page=sdcexec', label: 'Sponsored Research', target: '_blank' },
     ],
   },
