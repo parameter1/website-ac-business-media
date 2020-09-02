96c2246fbb25c99e275669ebe367d6b336cda509
Brandon Krigbaum <brandonbk@gmail.com>
2020-09-02 10:05:01 -0500

    Rename videos to published-videos for clarity

--- sites/greenindustrypros.com/server/routes/website-section.js
+++ sites/greenindustrypros.com/server/routes/website-section.js
@@ -1,7 +1,7 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('@ac-business-media/refresh-theme/templates/website-section');
 const podcasts = require('@ac-business-media/refresh-theme/templates/website-section/podcasts');
-const videos = require('@ac-business-media/refresh-theme/templates/website-section/videos');
+const publishedVideos = require('@ac-business-media/refresh-theme/templates/website-section/videos');
 const contactUs = require('@ac-business-media/refresh-theme/templates/website-section/contact-us');
 const queryFragment = require('@ac-business-media/refresh-theme/graphql/fragments/website-section-page');
 
@@ -21,7 +21,7 @@ module.exports = (app) => {
     queryFragment,
   }));
   app.get('/:alias(videos)', withWebsiteSection({
-    template: videos,
+    template: publishedVideos,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
