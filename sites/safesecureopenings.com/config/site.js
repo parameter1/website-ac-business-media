337360985ce3bd497c730060c8d170f280c32a7e
Josh Worden <josh@parameter1.com>
2020-12-09 11:49:10 -0600

    Add SSO site

---
+++ sites/safesecureopenings.com/config/site.js
@@ -0,0 +1,72 @@
+const navigation = require('./navigation');
+const gam = require('./gam');
+const nativeX = require('./native-x');
+const dragonForms = require('./dragon-forms');
+const events = require('./events');
+
+module.exports = {
+  gam,
+  nativeX,
+  dragonForms,
+  navigation,
+  events,
+  company: 'AC Business Media, LLC',
+  logos: {
+    navbar: {
+      src: 'https://img.safesecureopenings.com/files/base/acbm/fcp/image/static/sso/Logo-Final.png?h=45&auto=format,compress&q=70',
+      srcset: [
+        'https://img.safesecureopenings.com/files/base/acbm/fcp/image/static/sso/Logo-Final.png?h=90&auto=format,compress&q=70 2x',
+      ],
+    },
+    footer: {
+      src: 'https://img.safesecureopenings.com/files/base/acbm/fcp/image/static/sso/Logo-Final.png?h=60&auto=format,compress&q=70',
+      srcset: [
+        'https://img.safesecureopenings.com/files/base/acbm/fcp/image/static/sso/Logo-Final.png?h=120&auto=format,compress&q=70 2x',
+      ],
+    },
+  },
+  socialMediaLinks: [
+    { provider: 'linkedin', href: 'https://www.linkedin.com/company/fordsspros/', target: '_blank' },
+    { provider: 'twitter', href: 'https://twitter.com/fordsspros', target: '_blank' },
+    { provider: 'facebook', href: 'https://www.facebook.com/fordsspros', target: '_blank' },
+    // { provider: 'instagram', href: 'https://www.instagram.com/fordsspros/', target: '_blank' },
+  ],
+  radix: {
+    enabled: false,
+    // url: 'https://radix.base.parameter1.com',
+    // appId: '436dd265-a7ac-4608-8331-d5ea70e65102',
+    // submissionFieldIds: [
+    //   { name: 'comments', id: '5b99725a81284d4fc4e58233' },
+    // ],
+  },
+  gtm: {
+    containerId: 'GTM-M92TRPK',
+  },
+  // mediaFuse: {
+  //   id: 6062,
+  // },
+  gcse: {
+    id: '003355913687346718228:zrqhmh7lvw4',
+  },
+  wufoo: {
+    userName: 'acbm',
+  },
+  magazines: {
+    description: '',
+  },
+  contactUs: {
+    branding: {
+      bgColor: '#164f77',
+      logo: 'https://img.safesecureopenings.com/files/base/acbm/fcp/image/static/sso/Logo-Final.png?h=60&auto=format,compress&q=70',
+    },
+    to: 'jsage@acbusinessmedia.com',
+  },
+  inquiry: {
+    enabled: true,
+    directSend: true,
+    sendTo: 'jsage@acbusinessmedia.com',
+    sendFrom: 'GreenIndustryPros.com <noreply@baseplatform.io>',
+    logo: 'https://img.safesecureopenings.com/files/base/acbm/fcp/image/static/sso/Logo-Final.png?h=60&auto=format,compress&q=70',
+    bgColor: '#164f77',
+  },
+};
---
+++ sites/safesecureopenings.com/server/routes/website-section.js
@@ -0,0 +1,31 @@
+const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
+const section = require('@ac-business-media/refresh-theme/templates/website-section');
+const podcasts = require('@ac-business-media/refresh-theme/templates/website-section/podcasts');
+const publishedVideos = require('@ac-business-media/refresh-theme/templates/website-section/published-videos');
+const contactUs = require('@ac-business-media/refresh-theme/templates/website-section/contact-us');
+const queryFragment = require('@ac-business-media/refresh-theme/graphql/fragments/website-section-page');
+
+const directory = require('../templates/website-section/directory');
+
+module.exports = (app) => {
+  app.get('/:alias(directory)', withWebsiteSection({
+    template: directory,
+    queryFragment,
+  }));
+  app.get('/:alias(contact-us)', withWebsiteSection({
+    template: contactUs,
+    queryFragment,
+  }));
+  app.get('/:alias(podcasts)', withWebsiteSection({
+    template: podcasts,
+    queryFragment,
+  }));
+  app.get('/:alias(videos)', withWebsiteSection({
+    template: publishedVideos,
+    queryFragment,
+  }));
+  app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
+    template: section,
+    queryFragment,
+  }));
+};
