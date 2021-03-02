ba33de096e1f3cab1a360bc4fb1b1988e4b61d61
Brian Miller <brian@parameter1.com>
2021-03-02 12:48:10 -0600

    add components to browser and tag list

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,5 +1,7 @@
 import ContactUsForm from './contact-us-form.vue';
 import AutoScroll from './auto-scroll.vue';
+import CommonAlgoliaCompanySearch from './algolia-company-search.vue';
+import CommonSectionSearch from './section-search.vue';
 
 const SpecGuideTable = () => import(/* webpackChunkName: "common-spec-guide-table" */ './spec-guide/table.vue');
 
@@ -8,4 +10,6 @@ export default (Browser) => {
   Browser.registerComponent('CommonContactUsForm', ContactUsForm);
   Browser.registerComponent('CommonAutoScroll', AutoScroll);
   Browser.registerComponent('CommonSpecGuideTable', SpecGuideTable);
+  Browser.registerComponent('CommonAlgoliaCompanySearch', CommonAlgoliaCompanySearch);
+  Browser.registerComponent('CommonSectionSearch', CommonSectionSearch);
 };
