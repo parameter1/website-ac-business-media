e055940cf5eea70399c38e401f9157b7020eee88
Brian Miller <brian@parameter1.com>
2021-05-12 11:04:12 -0500

    Update p1-events to base-cms & add required files

---
+++ packages/refresh-theme/config/omeda.js
@@ -0,0 +1,9 @@
+module.exports = {
+  brandKey: 'hcl',
+  appId: process.env.OMEDA_APP_ID,
+  inputId: process.env.OMEDA_INPUT_ID,
+  graphqlUri: 'https://graphql.omeda.parameter1.com/',
+  rapidIdentification: {
+    productId: 15289,
+  },
+};
