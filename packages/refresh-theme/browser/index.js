ea9107c19932297e815b2e55d9fcb47f16780f3f
Brian Miller <brian@parameter1.com>
2021-05-13 13:09:45 -0500

    Add Browser components for IdentityX and OmedaRapidId
     - Add the Browser components for IdentityX ?
     — Authenticate
     — Comments Create, Login & Stream

--- packages/refresh-theme/browser/index.js
+++ packages/refresh-theme/browser/index.js
@@ -2,6 +2,7 @@ import DefaultTheme from '@parameter1/base-cms-marko-web-theme-default/browser';
 import GTM from '@parameter1/base-cms-marko-web-gtm/browser';
 import GAM from '@parameter1/base-cms-marko-web-gam/browser';
 import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
+import IdentityX from '@parameter1/base-cms-marko-web-identity-x/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 import Common from '@ac-business-media/package-common/browser';
@@ -9,6 +10,10 @@ import SocialSharing from '@parameter1/base-cms-marko-web-social-sharing/browser
 import PhotoSwipe from '@parameter1/base-cms-marko-web-photoswipe/browser';
 import RevealAd from '@parameter1/base-cms-marko-web-reveal-ad/browser';
 import Radix from '@parameter1/base-cms-marko-web-radix/browser';
+import OmedaRapidIdentityX from '@parameter1/base-cms-marko-web-omeda-identity-x/browser/rapid-identify.vue';
+
+const IdentityXAuthenticate = () => import(/* webpackChunkName: "refresh-theme-identity-x-authenticate" */ './identity-x/authenticate.vue');
+const IdentityXCommentStream = () => import(/* webpackChunkName: "refresh-theme-identity-x-comment-stream" */ './identity-x/comments/stream.vue');
 
 export default (Browser) => {
   DefaultTheme(Browser);
@@ -22,4 +27,19 @@ export default (Browser) => {
   PhotoSwipe(Browser);
   RevealAd(Browser);
   Radix(Browser);
+  IdentityX(Browser, {
+    CustomAuthenticateComponent: IdentityXAuthenticate,
+    CustomCommentStreamComponent: IdentityXCommentStream,
+  });
+
+  Browser.register('OmedaRapidIdentityX', OmedaRapidIdentityX, {
+    on: {
+      'encrypted-id-found': (encryptedId) => {
+        if (encryptedId && window.p1events) {
+          window.p1events('setIdentity', `omeda.${omedaConfig.brandKey}.customer*${encryptedId}~encrypted`);
+        }
+      },
+    },
+  });
+
 };
