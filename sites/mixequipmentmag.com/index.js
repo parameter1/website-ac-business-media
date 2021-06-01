6567e7ab5c98e4a6a285148841ff4961920079c6
Brian Miller <brian@parameter1.com>
2021-06-01 10:47:44 -0500

    Add site map to mixequipmentmag

--- sites/mixequipmentmag.com/index.js
+++ sites/mixequipmentmag.com/index.js
@@ -1,13 +1,23 @@
 const newrelic = require('newrelic');
 const { startServer } = require('@parameter1/base-cms-marko-web');
 const cleanResponse = require('@parameter1/base-cms-marko-core/middleware/clean-marko-response');
+const htmlSitemapPagination = require('@parameter1/base-cms-marko-web-html-sitemap/middleware/paginated');
+const htmlSitemapRoutes = require('@parameter1/base-cms-marko-web-html-sitemap/routes');
+
 const { version } = require('./package.json');
-const routes = require('./server/routes');
+const siteRoutes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
 const document = require('./server/components/document');
 const components = require('./server/components');
 const fragments = require('./server/fragments');
+
+const routes = (app) => {
+  // HTML Sitemap
+  htmlSitemapRoutes(app);
+  // Load site routes.
+  siteRoutes(app);
+};
 
 const { log } = console;
 
@@ -22,6 +32,10 @@ module.exports = startServer({
   version,
   onStart: (app) => {
     app.set('trust proxy', 'loopback, linklocal, uniquelocal');
+
+    // Use paginated middleware
+    app.use(htmlSitemapPagination());
+
     // Clean all response bodies.
     app.use(cleanResponse());
   },
